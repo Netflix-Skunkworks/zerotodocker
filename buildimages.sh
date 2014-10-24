d6c902e1434d2554d51441235fed6f5d7f296583
Andrew Spyker <awspyker@gmail.com>
2014-10-24 10:49:54 -0700

    fix up base image reference names

--- buildimages.sh
+++ buildimages.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
 
 cd java/7
-docker build -t netflixoss-java:7 .
+docker build -t netflixoss/java:7 .
 cd ../..
 
 cd tomcat/7
-docker build -t netflixoss-tomcat:7 .
+docker build -t netflixoss/tomcat:7 .
 cd ../..
 
 
 
 cd asgard
-docker build -t netflixoss-asgard:1.5 .
+docker build -t netflixoss/asgard:1.5 .
 cd ..
 
 cd eureka
-docker build -t netflixoss-eureka:1.1.142 .
+docker build -t netflixoss/eureka:1.1.142 .
 cd ..
 
 cd exhibitor-zookeeper
-docker build -t netflixoss-exhibitor:1.5.2 .
+docker build -t netflixoss/exhibitor:1.5.2 .
 cd ..
 
 cd zuul
-docker build -t netflixoss-zuul:1.0.28 .
+docker build -t netflixoss/zuul:1.0.28 .
 cd ..
 
 cd hello-netflix-oss
-docker build -t netflixoss-hello-netflix-oss:1.0.27 .
+docker build -t netflixoss/hello-netflix-oss:1.0.27 .
 cd ..
 
 # TODO Cassandra/Priam
